c1f54fe507458713c22eb5077308ba68b0c55cef
YmnPich <monypichyun.ymn@gmail.com>
2024-09-12 18:00:12 +0700

    7commit

--- pages/e-invitation/couple2.vue
+++ pages/e-invitation/couple2.vue
@@ -8,11 +8,11 @@
         
               <!--Slider Wrapper-->
            <div class="mx-auto w-auto relative">
-              <button id="slider-button-left" class="top-[35%] swiper-button-prev w-11 h-11 p-3 xl:flex hidden shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] items-center justify-center border border-gray-300 rounded-lg group hover:bg-gray-900 transition-all duration-700 ease-in-out">
+              <!-- <button id="slider-button-left" class="top-[35%] swiper-button-prev w-11 h-11 p-3 xl:flex hidden shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] items-center justify-center border border-gray-300 rounded-lg group hover:bg-gray-900 transition-all duration-700 ease-in-out">
                 <svg class="text-gray-900 group-hover:text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                   <path d="M12.5002 14.9999L7.50005 9.99973L12.5032 4.99658" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
-              </button>
+              </button> -->
               <div class="swiper-container gallery-top w-full md:w-[1028px] mx-auto xl:overflow-hidden pt-6">
                 <div class="swiper-wrapper w-max mx-auto mb-5 flex gap-3">
                   <div class="swiper-slide max-w-[319px] grid">
@@ -89,11 +89,11 @@
                   </div>
                 </div>
               </div>
-              <button id="slider-button-right" class="swiper-button-next top-[35%] xl:flex hidden shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] w-11 h-11 items-center justify-center border border-gray-300 rounded-lg group  transition-all duration-700 ease-in-out hover:bg-gray-900">
+              <!-- <button id="slider-button-right" class="swiper-button-next top-[35%] xl:flex hidden shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] w-11 h-11 items-center justify-center border border-gray-300 rounded-lg group  transition-all duration-700 ease-in-out hover:bg-gray-900">
                 <svg class="text-gray-900 group-hover:text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                   <path d="M7.50301 4.99637L12.5032 9.99657L7.50006 14.9997" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
                   </svg>
-              </button>
+              </button> -->
             </div>
   </div>
 
@@ -104,48 +104,48 @@
   import Swiper from 'swiper'; // Import Swiper.js
   
   export default {
-    mounted() {
-      // Initialize Swiper after the component mounts
-      this.initSwiper();
-    },
-    methods: {
-      initSwiper() {
-        const swiper = new Swiper('.gallery-top', {
-          spaceBetween: 20,
-          slidesPerView: 3,
-          parallax: true,
-          centeredSlides: true,
-          loop: true,
-          autoplay: true,
-          autoplaySpeed: 2000, // Adjust autoplay speed (milliseconds)
-          slideToClickedSlide: true,
-          paginationClickable: true,
-          navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
+  mounted() {
+    // Initialize Swiper after the component mounts
+    this.initSwiper();
+  },
+  methods: {
+    initSwiper() {
+      const swiper = new Swiper('.gallery-top', {
+        spaceBetween: 20,
+        slidesPerView: 3,
+        parallax: true,
+        centeredSlides: true,
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 2000, // Adjust autoplay speed (milliseconds)
+        slideToClickedSlide: true,
+        paginationClickable: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        breakpoints: {
+          1920: {
+            slidesPerView: 3,
+            spaceBetween: 20,
           },
-          breakpoints: {
-            1920: {
-              slidesPerView: 3,
-              spaceBetween: 20,
-            },
-            1400: {
-              slidesPerView: 3,
-              spaceBetween: 20,
-              centeredSlides: true
-            },
-            900: {
-              slidesPerView: 3,
-              spaceBetween: 15,
-              centeredSlides: true
-            },
-            200: {
-              slidesPerView: 2,
-              spaceBetween: 15
-            }
+          1400: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+            centeredSlides: true
           },
-        });
-      }
+          900: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+            centeredSlides: true
+          },
+          200: {
+            slidesPerView: 2,
+            spaceBetween: 15
+          }
+        },
+      });
     }
-  };
+  }
+};
   </script>
